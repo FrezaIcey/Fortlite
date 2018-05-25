0ac7e4dfe48e4e5bfb89f66d08d0cc13b4470027
mnewtonjr <39384233+mnewtonjr@users.noreply.github.com>
2018-05-25 14:08:49 -0400

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,25 @@
+public class Player()
+{
+    private int health, shield;
+  
+    public Player()
+    {
+        health = 100;
+        shield = 0;
+    }
+    
+    public int getHealth()
+    {
+        return health;
+    }
+    
+    public int getShield()
+    {
+        return shield;
+    }
+    
+    public void setHealth()
+    {
+        
+    }
+}
