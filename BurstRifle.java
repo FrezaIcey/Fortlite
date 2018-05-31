54022bcd3c53a56f52ab06b9ce5d7da9133a56f8
FrezaIcey <39065397+FrezaIcey@users.noreply.github.com>
2018-05-30 22:27:21 -0400

    Update BurstRifle.java

--- BurstRifle.java
+++ BurstRifle.java
@@ -1,20 +1,13 @@
-//ChrisEden 5/18/18 Version: 0.1
+//ChrisEden 5/30/18 Version: 0.2
 public class BurstRifle extends Weapon 
 {
 	private int damageBR, magazineBR, currentMagazine;
 	private String name; 
 	public BurstRifle()
 	{
-		  damageBR = 20; 
+		  damageBR = 15; 
 		  magazineBR = 30; 
 		  currentMagazine = magazineBR; 
-		  name = "Burst Rifle";
-	}
-	public BurstRifle(int c)
-	{
-		  damageBR = 20; 
-		  magazineBR = 30; 
-		  currentMagazine = c; 
 		  name = "Burst Rifle";
 	}
 	public String getName()
@@ -32,6 +25,10 @@ public class BurstRifle extends Weapon
 	public int getCurrentMagazine()
 	{
 		return currentMagazine; 
+	}
+	public int getDistance() 
+	{
+		return 342; 
 	}
 }
 
