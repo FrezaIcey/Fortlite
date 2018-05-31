073c21f9f897d052edacfc5fc4f50c23e9523d5a
FrezaIcey <39065397+FrezaIcey@users.noreply.github.com>
2018-05-30 22:27:48 -0400

    Update Fists.java

--- Fists.java
+++ Fists.java
@@ -1,27 +1,31 @@
-
+// ChrisEden 5/30/18 Version 0.2
 public class Fists extends Weapon
 {
-		  private String name; 
-		  private int damage; 
-		  public Fists()
-		  {
-		  	  name = "Fists" 
-		  		damage = 10; 
-		  }
-		  public String getName()
-		  {
-		   		return name; 
-		  }
-		  public int getDamage()
-		  {
-		      return damage; 
-		  }
-		  public int getmagazineSize()
-		  {
-		  		return -1; 
-		  }
-		  public int getCurrentMagazine()
-		  {
-		  		return -1;
-		  }
+	private String name; 
+	private int damage; 
+	public Fists()
+	{
+		name = "Fists";
+		damage = 100; 
+	}
+	public String getName()
+	{
+		return name; 
+	}
+	public int getDamage()
+	{
+	    return damage; 
+	}
+    public int getMagazineSize()
+	{
+		return -1; 
+	}
+	public int getCurrentMagazine()
+	{
+		return -1;
+	}
+	public int getDistance() 
+	{
+		return 10; 
+	}
 }
