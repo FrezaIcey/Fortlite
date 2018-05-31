0ffc41991a852f8bf73cd41bea05e7fdd0906020
FrezaIcey <39065397+FrezaIcey@users.noreply.github.com>
2018-05-30 23:11:45 -0400

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -1,15 +1,27 @@
-//ChrisEden 5/21/18 Version: 0.1
+// ChrisEden 5/30/18 Version 0.2
 public class Bullet 
 {
-	private int damage, posX, posY; 
+	private int damage, distance, xPos, yPos; 
+	private Position p; 
 	public Bullet(int x, int y, Weapon w)
 	{
-		posX = x; 
-		posY = y; 
-		damage = w.getDamage(); 
+		p = new Position(x,y); 
+		xPos = x; 
+		yPos = y; 
+		damage = w.getDamage();
+		distance = w.getDistance(); 
 	}
-//	public int setPosition()
+	public Position setPosition(int x, int y)
+	{ 
+		p = new Position(xPos + x, yPos + y);
+		return p; 
+	}
+	public int getDamage()
 	{
-		
+		return damage; 
+	}
+	public int getDistance()
+	{
+		return distance; 
 	}
 }
