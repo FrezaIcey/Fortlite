a2239902172cd82d1b2b9fb5fb74fb0b6f858ace
FrezaIcey <39065397+FrezaIcey@users.noreply.github.com>
2018-05-30 23:13:05 -0400

    Update Position.java

--- Position.java
+++ Position.java
@@ -1,4 +1,18 @@
-
-public class Position {
-
+// ChrisEden 5/30/18 Version 0.1
+public class Position 
+{
+	private int xPos, yPos; 
+	public Position(int x, int y)
+	{
+		xPos = x; 
+		yPos = y; 
+	}
+	public int getX()
+	{
+		return xPos; 
+	}
+	public int getY()
+	{
+		return yPos; 
+	}
 }
