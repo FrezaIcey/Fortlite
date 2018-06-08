8d7a32a35f3293709469bd7c384ef5e61aac6585
FrezaIcey <39065397+FrezaIcey@users.noreply.github.com>
2018-06-08 13:10:52 -0400

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -1,5 +1,9 @@
-// ChrisEden 5/30/18 Version 0.2
+package dev.project.fortlite.weapon;
 
+// ChrisEden 6/8/18 Version: 0.4;
+import java.awt.event.*;
+import javax.swing.Timer; 
+import edu.wpi.first.wpilibj.XboxController;
 public abstract class Weapon 
 {
 	public abstract String getName(); 
@@ -11,6 +15,8 @@ public abstract class Weapon
 	public abstract void addAmmo(); 
 	public abstract int getVelocity();
 	
+	public static boolean reloading = false; 
+		
 	public int fire(Weapon w)
 	{
 		if(canShoot(w))
@@ -22,17 +28,41 @@ public abstract class Weapon
 	}
 	private boolean canShoot(Weapon w)
 	{
-		if(w.getName() != "Burst Rifle")
+		if(!reloading)
 		{
-			if(w.getCurrentMagazine() != 0)
-				return true; 
-			return false; 
+			if(w.getName() != "Burst Rifle")
+			{
+				if(w.getCurrentMagazine() != 0)
+					return true;  
+			}
+			else 
+			{
+				if(w.getCurrentMagazine() >= 3)
+					return true; 
+			}
 		}
-		else 
+		return false; 
+	}
+	public void updateWeapon(Weapon w, XboxController x)
+	{
+		//creates a timer object that whenever there is user input on the x button, it sets reloading to true 
+		//and doesn't allow for other input
+		Timer t = new Timer(w.getReloadTime() * 1000, new ActionListener() 
 		{
-			if(w.getCurrentMagazine() >= 3)
-				return true; 
-			return false; 
+		     public void actionPerformed(new ActionEvent(x, 3, "Reload"))
+		     {
+		    	 if(a.getModifiers() == 3)
+		    	 { 
+		    		 t.start(); 
+		    		 reloading = true;
+		    		 w.addAmmo();
+		    	 }
+		     }
+		}); 
+		
+		if(reloading && t.isRunning())
+		{	
+			t.stop(); 
 		}
 	}
 }
